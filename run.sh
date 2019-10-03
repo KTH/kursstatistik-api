7afd7d9e9beced5a749a286911b745c9722cb081
Elena Rakhimova <elenara@kth.se>
2019-10-03 17:20:56 +0200

    Replaced chonny with kursstatistik-api

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo $LADOK3_CERT | base64 -d > ./config/secrets/chonny@KTH.crt && chmod 600 ./config/secrets/chonny@KTH.crt
-echo $LADOK3_CERT_KEY | base64 -d > ./config/secrets/chonny@KTH.key && chmod 600 ./config/secrets/chonny@KTH.key
+echo $LADOK3_CERT | base64 -d > ./config/secrets/kursstatistik-api@KTH.crt && chmod 600 ./config/secrets/kursstatistik-api@KTH.crt
+echo $LADOK3_CERT_KEY | base64 -d > ./config/secrets/kursstatistik-api@KTH.key && chmod 600 ./config/secrets/kursstatistik-api@KTH.key
 echo $CA_FILE | base64 -d > ./config/secrets/UF-prod-ca-bundle.txt && chmod 600 ./config/secrets/UF-prod-ca-bundle.txt
 
 
